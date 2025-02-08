0387f49f2655b5e1dab38567092c73fc31f660db
Sharukh Sid <139120820+Sharukh0010@users.noreply.github.com>
2025-02-08 11:18:39 +0530

    Update credit_card_part_2.py

--- credit_card_part_2.py
+++ credit_card_part_2.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1rbkM323cjQaGJCQg19zZKECnLOuVrvyY
 """
 
-!pip install streamlit
+# !pip install streamlit
 
 import streamlit as st
 import pandas as pd
